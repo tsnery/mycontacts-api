2d93478016915f2ce64f728b38ec34e5826dfc20
Tainan Nery <nerytaynan@gmail.com>
2022-07-13 23:29:56 -0300

    feat: connection to database

---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import {Client} from 'pg'
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'mycontacts',
+})
+
+client.connect()
