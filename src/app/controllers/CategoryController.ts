533db99a9591a310b8b27a21db0c0ca7f12127ab
Tainan Nery <zaoldyeck@outlook.com>
2022-07-20 23:52:21 -0300

    feat: store and list categories

--- src/app/controllers/CategoryController.ts
+++ src/app/controllers/CategoryController.ts
@@ -1,12 +1,23 @@
 import {Request, Response} from 'express'
+import CategoryRepository from '../repositories/CategoryRepository'
 
 class CategoryController {
-  index(request: Request, response: Response) {
-    response.json({ok: 'ok'})
+  async index(request: Request, response: Response) {
+    const categories = await CategoryRepository.findAll()
+
+    response.json(categories)
   }
 
-  store(request: Request, response: Response) {
-    response.json({ok: true})
+  async store(request: Request<{}, {}, {name: string}>, response: Response) {
+    const {name} = request.body
+
+    if (!name) {
+      return response.status(400).json({error: 'Name is required!'})
+    }
+
+    const category = await CategoryRepository.create({name})
+
+    response.json(category)
   }
 }
 
--- src/app/repositories/CategoryRepository.ts
+++ src/app/repositories/CategoryRepository.ts
@@ -2,16 +2,17 @@ import db from '../../database'
 import {TCategory, TCreateCategoryProps} from './types'
 
 class CategoryRepository {
-  async findAll() {
-    const [rows] = await db.query(`SELECT * FROM categories`)
+  async findAll(): Promise<TCategory[]> {
+    const rows = await db.query(`SELECT * FROM categories ORDER BY name`)
 
     return rows
   }
 
   async create({name}: TCreateCategoryProps): Promise<TCategory> {
-    const [row] = await db.query(`INSERT INTO categories(name) VALUES($1)`, [
-      name,
-    ])
+    const [row] = await db.query(
+      `INSERT INTO categories(name) VALUES($1) RETURNING *`,
+      [name],
+    )
 
     return row
   }
